824b36887fb77acfafa713aba1b5acb60523ded7
rojaberi <beriroja18@gmail.com>
2026-04-02 23:14:30 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href'))
+            .scrollIntoView({
+                behavior: 'smooth'
+            });
+    });
+});
